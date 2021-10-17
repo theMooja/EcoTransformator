1d8a979bb9ca5ca5b8b97ddabc0baeb6e233afa2
theMooja <the.mooja@gmail.com>
2021-10-17 09:25:15 +0200

    Initial setup
    Initial setup

---
+++ EcoTransformator/ElectricEngine/ElectricEngineItem.cs
@@ -0,0 +1,28 @@
+﻿using Eco.Gameplay.Items;
+using Eco.Mods.TechTree;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Serialized]
+[Currency]
+[MaxStackSize(1)]
+[LocDisplayName("Electric engine")]
+
+public partial class ElectricEngineItem : WorldObjectItem<ElectricEngineObject>
+{
+    public override LocString DisplayDescription
+    {
+        get { return Localizer.DoStr("Electric engine"); }
+    }
+
+
+    static ElectricEngineItem()
+    {
+
+    }
+}
---
+++ EcoTransformator/ElectricEngine/ElectricEngineObject.cs
@@ -0,0 +1,68 @@
+﻿using Eco.Gameplay.Components;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Skills;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace Eco.Mods.TechTree
+{
+    [Serialized]
+    [RequireComponent(typeof(SolidGroundComponent))]
+    public partial class ElectricEngineObject : WorldObject, IRepresentsItem
+    {
+        public override LocString DisplayName
+        {
+            get { return Localizer.DoStr("Electric engine"); }
+        }
+        public override LocString DisplayDescription
+        {
+            get { return Localizer.DoStr("Object Description"); }
+        }
+
+        public Type RepresentedItemType { get { return typeof(ElectricEngineItem); } }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        static ElectricEngineObject() { }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
+    }
+
+    
+    /*
+    [RequiresSkill(typeof(IndustrySkill), 3)]
+    public partial class ElectricEngineRecipe : RecipeFamily
+    {
+        public ElectricEngineRecipe()
+        {
+            // this.Initialize(Localizer.DoStr("Recipe string"), typeof(ElectricEngineRecipe));
+            this.Recipes = new List<Recipe>
+            {
+                new Recipe(
+                    "ElectricEngine",
+                    Localizer.DoStr("Recipe text"),
+                    new IngredientElement[]
+                    {
+                        new IngredientElement(typeof(IronGearItem), 1)
+                    },
+                    new CraftingElement<ElectricEngineItem>())
+            };
+
+            //this.LaborInCalories = CreateLaborInCaloriesValue(500, typeof(IndustrySkill));
+            //this.ExperienceOnCraft = 5;
+            //this.CraftMinutes = CreateCraftTimeValue(typeof(ElectricEngineRecipe), 5, typeof(IndustrySkill));
+            // CraftingComponent.AddRecipe(typeof(WorkbenchObject), this);
+        }
+    }
+    */
+
+}
